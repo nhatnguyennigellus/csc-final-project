03cd85ca868ae3cb1d890370cb098148ac9c36c9
sonyynosification <tpvinhcse@gmail.com>
2015-04-21 11:34:01 +0700

    Add test-context.xml

--- FinalProject/src/test/java/csc/fresher/finalproject/service/AccountServiceTest.java
+++ FinalProject/src/test/java/csc/fresher/finalproject/service/AccountServiceTest.java
@@ -4,26 +4,26 @@ import java.text.DateFormat;
 
 import javax.swing.text.DateFormatter;
 
+import junit.framework.Assert;
+
 import org.easymock.EasyMock;
 import org.easymock.EasyMockRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import csc.fresher.finalproject.domain.SavingAccount;
 
-@RunWith(EasyMockRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:/META-INF/test-context.xml"})
 public class AccountServiceTest {
 	@Autowired
 	private BankingService bankingService;
 	
 	@Test
 	public void testAccountService() {
-//		SavingAccount savingAccount = new SavingAccount();
-//		savingAccount.setAccountNumber("150417000001");
-//		savingAccount.setAccountOwner("Vinh TP");
-//		savingAccount.setBalanceAmount(10000000);
-//		savingAccount.setInterest();
-//		savingAccount.setDueDate(new Date());
+		Assert.assertNotNull(bankingService);
 	}
 }
