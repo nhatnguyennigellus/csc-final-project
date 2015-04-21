3964bc45a85853ec1edb361f8a251169b07d4b9e
sonyynosification <tpvinhcse@gmail.com>
2015-04-21 11:38:58 +0700

    fix using @Before

--- FinalProject/src/test/java/csc/fresher/finalproject/service/AccountServiceTest.java
+++ FinalProject/src/test/java/csc/fresher/finalproject/service/AccountServiceTest.java
@@ -8,12 +8,14 @@ import junit.framework.Assert;
 
 import org.easymock.EasyMock;
 import org.easymock.EasyMockRunner;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import csc.fresher.finalproject.controller.EntityManagerFactoryUtil;
 import csc.fresher.finalproject.domain.SavingAccount;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -22,8 +24,14 @@ public class AccountServiceTest {
 	@Autowired
 	private BankingService bankingService;
 	
+	@Before
+	public void setUp() {
+		EntityManagerFactoryUtil.setEntityManagerFactory();
+	}
+	
 	@Test
 	public void testAccountService() {
 		Assert.assertNotNull(bankingService);
+		bankingService.existedAccountNumber("2015");
 	}
 }
